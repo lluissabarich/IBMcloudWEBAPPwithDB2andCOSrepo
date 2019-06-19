4442b00887c976fcf5fac0c4c6ed1d39e92acd23
Luis Sabarich <sabarich@gmail.com>
2019-06-19 15:43:11 +0200

    para almirall 2

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def input():
 
 @app.route('/')
 def helloworld():
-	return ("App Up -- modificacion para la demo de hoy en Almirall")
+	return ("App Up -- modificacion para la demo de hoy en Almirall2")
 
 @app.route('/result',methods = ['POST'])
 def result():
